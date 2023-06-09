c2c331f0e992b2ca905885474f09e7a77ee69beb
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-06-09 21:20:23 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require('dotenv').config();
 
-mongoose.connect("mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@data.fvqv3iu.mongodb.net/", {
+mongoose.connect("mongodb+srv://fotiosvaitsopoulos:Quietcrow561821@data.fvqv3iu.mongodb.net/", {
     useNewUrlParser: true
 });
 
